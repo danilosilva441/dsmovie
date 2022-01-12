112abfd362250fd83528af0371245ea6cff54e24
Danilo da Silva Souza <danilosilva441@gmail.com>
2022-01-12 13:19:17 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class Movie implements Serializable {
 	private Integer count;
 	private String image;
 
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
+	
 	public Movie() {
 	}
 
@@ -71,5 +77,9 @@ public class Movie implements Serializable {
 
 	public void setImage(String image) {
 		this.image = image;
+	}
+
+	public Set<Score> getScores() {
+		return scores;
 	}
 }
--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +20,9 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
+
+	@OneToMany(mappedBy = "id.user")
+	private Set<Score> scores = new HashSet<>();
 	
 	public User() {
 	}
